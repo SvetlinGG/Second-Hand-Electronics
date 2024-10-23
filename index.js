2e761a27a9eddf49b139bb9f0baef28268089102
SvetlinGarabedyan <svetlingarabedyan@yahoo.com>
2024-10-23 23:19:18 +0200

    set express

---
+++ index.js
@@ -0,0 +1,11 @@
+import express from 'express';
+
+
+
+const app = express();
+
+app.get('/', (req, res)=> {
+    res.send('Just works!');
+})
+
+app.listen(1000, () => console.log('Server is listening on http://localhost:1000'))
